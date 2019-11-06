d31e7ba6f01aee60ed2c0e95bf7b10cd8270cfe0
Monika-Das <monika.das8184@gmail.com>
2019-11-06 09:16:10 +0530

    First commit for project creation

---
+++ src/main/scala/com/scalapractise/scalaAppMain.scala
@@ -0,0 +1,14 @@
+package com.scalapractise
+
+/**
+  * created on 6/11, argument arg will take an array of strings, main is name of method & Unit is saying that
+  * this method returns nothing -- same as void in java
+*/
+
+object scalaAppMain {
+
+  def main(args:Array[String]):Unit = {
+    println("Main method created")
+  }
+
+}
---
+++ src/main/scala/com/scalapractise/scalaExtendsApp.scala
@@ -0,0 +1,8 @@
+package com.scalapractise
+
+object scalaExtendsApp extends App {
+
+  println("This main is from App")
+  println("This is breakpoint for debug")
+
+}
